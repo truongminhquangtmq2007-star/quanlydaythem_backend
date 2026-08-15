a5070e5dfd18bd1629f64bbf5f636cb5b448f94a
truongminhquangtmq2007-star <truongminhquangtmq2007@gmail.com>
2026-08-15 13:16:06 +0700

    fixx db

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,18 +7,8 @@ console.log("Mật khẩu đang đọc được là:", process.env.DB_PASSWORD);
 
 // Khởi tạo một "hồ bơi" (Pool) chứa các kết nối tới cơ sở dữ liệu
 const pool = new Pool({
-
   connectionString: process.env.DATABASE_URL,
-  
-  // BẮT BUỘC PHẢI THÊM ĐOẠN SSL NÀY
-  ssl: {
-    rejectUnauthorized: false
-  },
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Kiểm tra kết nối
