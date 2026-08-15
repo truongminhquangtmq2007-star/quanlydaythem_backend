c564a30270216a0b37d36afda9d8b95efbde946d
truongminhquangtmq2007-star <truongminhquangtmq2007@gmail.com>
2026-08-15 13:34:35 +0700

    fixx env

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,12 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  // Bỏ ssl vì đang chạy local, không cần SSL với localhost
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 pool.connect()
