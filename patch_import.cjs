f0921c2aa63ca7db9ba11e42fa8773ffab2da019
truongminhquangtmq2007-star <truongminhquangtmq2007@gmail.com>
2026-08-24 16:45:22 +0700

    chore: remove patch scripts

--- patch_import.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/controllers/examController.ts', 'utf8');
-
-if (!code.includes('import { generateWithFallback }')) {
-  code = "import { generateWithFallback } from '../services/geminiService';\n" + code;
-  fs.writeFileSync('src/controllers/examController.ts', code);
-  console.log('Added import');
-}
-
