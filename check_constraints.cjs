d88cb94d6076f0107f90ecce58d71bfef94797a5
truongminhquangtmq2007-star <truongminhquangtmq2007@gmail.com>
2026-08-30 09:52:16 +0700

    fixdxxx

--- check_constraints.cjs
+++ check_constraints.cjs
@@ -4,4 +4,5 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-pool.query("SELECT conname, pg_get_constraintdef(c.oid) as def FROM pg_constraint c WHERE conrelid = 'documents'::regclass").then(res => console.log(res.rows)).finally(() => process.exit(0));
+pool.query("SELECT conname, pg_get_constraintdef(c.oid) as def FROM pg_constraint c WHERE conrelid = 'student_topic_performance'::regclass").then(res => console.log(res.rows)).finally(() => process.exit(0));
+
